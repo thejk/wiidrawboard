1cedbf8ba8a45be7d0d06ee15ec496123ed3684c
Mikael Modin <micke.modin@gmail.com>
2011-12-11 13:08:38 +0100

    Mindor cleanup

--- inc/hid.hpp
+++ inc/hid.hpp
@@ -10,38 +10,38 @@
 class HIDMgr;
 class HIDDevice {
 public:
-	HIDDevice(hid_device* device, HIDMgr* mgr)
-	: mDevice(device), mMgr(mgr) { }
+    HIDDevice(hid_device* device, HIDMgr* mgr)
+    : mDevice(device), mMgr(mgr) { }
 
-	~HIDDevice() {
-		if(mDevice != 0)
-			hid_close(mDevice);
-	}
+    ~HIDDevice() {
+        if(mDevice != 0)
+            hid_close(mDevice);
+    }
 
-	bool parseInfo(hid_device* dev = 0);
+    bool parseInfo(hid_device* dev = 0);
 
 private:
-	hid_device_info* getDevice(wchar_t* serial);
-	uint16_t parseWString(std::wstring str);
+    hid_device_info* getDevice(wchar_t* serial);
+    uint16_t parseWString(std::wstring str);
 
-	hid_device* mDevice;
+    hid_device* mDevice;
     std::wstring mSerial;
-	uint16_t mProduct;
+    uint16_t mProduct;
     std::wstring mManufacturer;
-	HIDMgr* mMgr;
+    HIDMgr* mMgr;
 };
 
 class HIDMgr
 {
 public:
-	HIDMgr();
-	~HIDMgr();
-	HIDDevice* open(uint16_t vendor, uint16_t product);
-	void updateDevicesList();
-	hid_device_info* getDevices() { return mDevices; }
+    HIDMgr();
+    ~HIDMgr();
+    HIDDevice* open(uint16_t vendor, uint16_t product);
+    void updateDevicesList();
+    hid_device_info* getDevices() { return mDevices; }
 
 private:
-	hid_device_info* mDevices;
+    hid_device_info* mDevices;
 };
 
 #endif /* HID_HPP */
--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,35 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	HIDMgr mgr;
-	uint16_t vendor = 0x057E;
+    HIDMgr mgr;
+    uint16_t vendor = 0x057E;
     uint16_t product = 0x0306;
-	const wchar_t* tmp_serial = L"E0-E7-51-97-74-09";
-	wchar_t* serial = new wchar_t[wcslen(tmp_serial)+1];
-	memcpy(serial, tmp_serial, (wcslen(tmp_serial)+1)*sizeof(wchar_t));
-	wcout <<"Serial: " <<wcslen(serial) <<" " <<serial <<endl;
-	
-	char pin[6];
-	pin[5] = 0x0;
-	pin[4] = 0x1;
-	pin[3] = 0xb;
-	pin[2] = 0xd0;
-	pin[1] = 0xea;
-	pin[0] = 0xe0;
-	
-	HIDDevice* device = mgr.open(vendor, product);
-	cout <<(device!=NULL?"success":"failure") <<endl;
-	
-	/*
-	hid_device_info* devs = hid_enumerate(0, 0);
-	int count = 0;
-	while(devs != 0) {
-		wcout <<"manufacturer: " <<devs->manufacturer_string <<endl;
-		cout <<"vendor: " <<hex <<devs->vendor_id <<endl;
-		cout <<"product: " <<hex <<devs->product_id <<endl;
-		devs = devs->next;
-		count++;
-	}
-	cout <<count <<" devices" <<endl;
-	hid_free_enumeration(devs)*/
+    const wchar_t* tmp_serial = L"E0-E7-51-97-74-09";
+    wchar_t* serial = new wchar_t[wcslen(tmp_serial)+1];
+    memcpy(serial, tmp_serial, (wcslen(tmp_serial)+1)*sizeof(wchar_t));
+    wcout <<"Serial: " <<wcslen(serial) <<" " <<serial <<endl;
+
+    char pin[6];
+    pin[5] = 0x0;
+    pin[4] = 0x1;
+    pin[3] = 0xb;
+    pin[2] = 0xd0;
+    pin[1] = 0xea;
+    pin[0] = 0xe0;
+
+    HIDDevice* device = mgr.open(vendor, product);
+    cout <<(device!=NULL?"success":"failure") <<endl;
+
+    /*
+    hid_device_info* devs = hid_enumerate(0, 0);
+    int count = 0;
+    while(devs != 0) {
+            wcout <<"manufacturer: " <<devs->manufacturer_string <<endl;
+            cout <<"vendor: " <<hex <<devs->vendor_id <<endl;
+            cout <<"product: " <<hex <<devs->product_id <<endl;
+            devs = devs->next;
+            count++;
+    }
+    cout <<count <<" devices" <<endl;
+    hid_free_enumeration(devs)*/
 }
